a72ad08b6f7aee19e09ff2bd1ad80b0b08feb672
KyleVong <121789633+kylvng@users.noreply.github.com>
2023-12-27 17:33:05 -0500

    initialized minimarket.db and opened db in main

--- main.c
+++ main.c
@@ -3,5 +3,15 @@
 #include <stdlib.h>
 
 int main(){
-  printf("a");
+  sqlite3 *db;
+  int rc = sqlite3_open("minimarket.db", &db);
+
+  if (rc != SQLITE_OK) {
+      printf("Cannot open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return 1;
+  }
+
+  printf("Hey\n");
+  return 0;
 }
